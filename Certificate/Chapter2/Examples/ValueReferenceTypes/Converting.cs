b0dd7ee469ea2d1c330602ef29db1fd5f80a34ea
Huseyn-PC <Huseyn-PC@DESKTOP-BGIBJHD>
2019-08-29 11:57:14 +0400

    converting examples was added

---
+++ Certificate/Chapter2/Examples/ValueReferenceTypes/Converting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certificate.Chapter2.Examples.ValueReferenceTypes
+{
+    public class Converting
+    {
+        public void ConvertImplicitly()
+        {
+            HttpClient client = new HttpClient();
+            object o = client;
+            IDisposable d = client;
+        }
+
+        public void ConvertExplicitly()
+        {
+            //value type
+            double x = 1234.7;
+            int a;
+            // Cast double to int
+            a = (int)x; // a = 1234
+
+            //reference type
+            object stream = new MemoryStream();
+            MemoryStream memoryStream = (MemoryStream)stream;
+
+        }
+
+
+
+    }
+}
---
+++ Certificate/Chapter2/Examples/ValueReferenceTypes/Money.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certificate.Chapter2.Examples.ValueReferenceTypes
+{
+    public class Money
+    {
+        public Money(decimal amount)
+        {
+            Amount = amount;
+        }
+        public decimal Amount { get; set; }
+        public static implicit operator decimal(Money money)
+        {
+            return money.Amount;
+        }
+        public static explicit operator int(Money money)
+        {
+            return (int)money.Amount;
+        }
+    }
+}
