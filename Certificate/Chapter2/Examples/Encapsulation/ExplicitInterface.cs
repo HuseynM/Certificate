8edc8dbf45ef7c180129d93a64c30c9ab6b1dba9
huseynm <huseyn.mikayil95@gmail.com>
2019-08-29 21:14:24 -0700

    explicit interface example was added

---
+++ Certificate/Chapter2/Examples/Encapsulation/ExplicitInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certificate.Chapter2.Examples.Encapsulation
+{
+    public class ExplicitInterface
+    {
+        public void Execute()
+        {
+            DbContext ctx = new DbContext("");
+            var objectContext = ((IObjectContextAdapter)ctx).ObjectContext;
+        }
+    }
+
+    public interface ILeft
+    {
+        void Move();
+    }
+    public interface IRight
+    {
+        void Move();
+    }
+
+    public class Movement : ILeft, IRight
+    {
+        void ILeft.Move() { }
+        void IRight.Move() { }
+
+    }
+}
