f01b9db025f0a6830bafa0f01ad8e3d1642be3bd
huseynm <huseyn.mikayil95@gmail.com>
2019-09-01 12:30:13 -0700

    icomparable interface was implemented

---
+++ Certificate/Chapter2/Examples/ImplementingInterfaces/OrderComparable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certificate.Chapter2.Examples.ImplementingInterfaces
+{
+    public class OrderComparable : IComparable
+    {
+        public DateTime Created { get; set; }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            OrderComparable order = obj as OrderComparable;
+        }
+    }
+}
